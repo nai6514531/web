a01e1d24612ebaa518ab59d1a12bf2a0e072091b
Table <biaoqier@qq.com>
2017-11-15 23:31:40 +0800

    客服系统增加 C 端用户钱包清零功能

--- src/static/application/apps/main/services/soda/wallet.js
+++ src/static/application/apps/main/services/soda/wallet.js
@@ -2,9 +2,10 @@ import request from '../../utils/request'
 
 const walletService = {
   getTotalValue: (options) => {
-    return request.get(`/wallet/total-value`, {
-    	params: options
-    })
+    return request.get(`/wallet/total-value`)
+  },
+  resetValue: (mobile) => {
+    return request.get(`/crm/wallet/${mobile}/actions/reset`)
   }
 }
 export default walletService
