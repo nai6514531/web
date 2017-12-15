1161ed44ff2a67e9996de3631592d4c33cbbb69c
Arco1ris <longhmail@gmail.com>
2017-12-15 18:07:34 +0800

    新增游戏列表页

---
+++ src/static/application/apps/main/routers/game/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import { getComponent } from '../../components/bundle/'
+
+import Game from 'bundle-loader?lazy!../../views/game/game/index.js'
+import gameModel from 'bundle-loader?lazy!../../models/game/game/index.js'
+
+import Supplier from 'bundle-loader?lazy!../../views/game/supplier/index.js'
+import supplierModel from 'bundle-loader?lazy!../../models/game/supplier/index.js'
+import SupplierEdit from 'bundle-loader?lazy!../../views/game/supplier/edit/index.js'
+
+export default function (history, app) {
+    return (
+      <Switch>
+        <Route exact path='/game/game' component={getComponent(Game, app, gameModel)} />
+
+        <Route exact path='/game/supplier' component={getComponent(Supplier, app, supplierModel)} />   
+        <Route exact path='/game/supplier/:id' component={getComponent(SupplierEdit, app, supplierModel)} />        
+      </Switch>
+    )
+  }
