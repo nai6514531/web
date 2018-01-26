61bea5bb142e0a1e151559708dfb688fc748e057
Table <biaoqier@qq.com>
2018-01-26 15:49:39 +0800

    增加商家公众号服务器认证

--- src/server/mounts.js
+++ src/server/mounts.js
@@ -1,5 +1,11 @@
 import mount from 'koa-mount'
 
 module.exports = function (app) {
+
+  app.use(mount('/MP_verify_Web8PBTLyZQ2Fyz8.txt', function (ctx) {
+    ctx.body = 'Web8PBTLyZQ2Fyz8'
+  }))
+
   app.use(mount('/', require('./application')))
+
 }
